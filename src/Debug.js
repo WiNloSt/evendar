492d899bf44ce63f1b2c0fdf0e4b301269ac09b7
lifez <lifez999@gmail.com>
2017-10-28 00:09:07 +0700

    Add react router and debug

---
+++ src/Debug.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+export default class Debug extends Component{
+
+  render () {
+  return (
+    <div>
+        <h1>THIS IS DEBUG</h1>
+    </div>
+    )
+  }
+}
