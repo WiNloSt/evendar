6b1ed78cdb23a7538d12b6722c46c1263255ca05
b5710546232 <nattapat.su@ku.th>
2017-10-28 03:03:58 +0700

    comment event carlist

--- src/Debug.js
+++ src/Debug.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import FriendTabs from './components/FriendTabs'
+import EventCardList from './components/EventCardList'
 export default class Debug extends Component{
 
   render () {
   return (
     <div>
         <h1>THIS IS DEBUG</h1>
-        <FriendTabs/>
+        
+        {<FriendTabs/>}
     </div>
     )
   }
