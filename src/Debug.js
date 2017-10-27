6e7397d521e174be75cc0d3ea6bddc486969447a
b5710546232 <nattapat.su@ku.th>
2017-10-28 02:19:32 +0700

    mock friendlist

--- src/Debug.js
+++ src/Debug.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import FriendTabs from './components/FriendTabs'
 export default class Debug extends Component{
 
   render () {
   return (
     <div>
         <h1>THIS IS DEBUG</h1>
+        <FriendTabs/>
     </div>
     )
   }
