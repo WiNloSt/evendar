10ace8cb26ac4dd224c5047c9c85892c3894c320
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-10-28 10:18:50 +0700

    Recommend

--- src/components/FriendTabs.js
+++ src/components/FriendTabs.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Tabs, Tab } from 'material-ui/Tabs'
 import FriendCardList from './FriendCardList'
-import { getEvents } from '../utils/services'
+import { getEvents, getAllTargetEvents } from '../utils/services'
+import _ from 'lodash';
 
 export default class FriendTabs extends React.Component {
   constructor(props) {
@@ -21,20 +22,23 @@ export default class FriendTabs extends React.Component {
 
   componentDidMount() {
     global.FB.getLoginStatus(()=> {
-      getEvents().then((_events) => {
-        const events = (
-          _events.data.map((e) => {
-            return {
-              name: e.name,
-              id: e.id,
-              imgUrl: e.picture.data.url,
-              going: e.rsvp_status
-            }
+      getAllTargetEvents().then((recommendEvents) => {
+        getEvents().then((_events) => {
+          const events = (
+            _events.data.map((e) => {
+              return {
+                name: e.name,
+                id: e.id,
+                imgUrl: e.picture.data.url,
+                going: e.rsvp_status
+              }
+            })
+          )
+          this.setState({
+            goingEvents: events.filter(e => e.going),
+            interestedEvents: events.filter(e => !e.going),
+            recommendEvents: _.map(recommendEvents, 'data')
           })
-        )
-        this.setState({
-          goingEvents: events.filter(e => e.going),
-          interestedEvents: events.filter(e => !e.going)
         })
       })
     })
@@ -43,6 +47,11 @@ export default class FriendTabs extends React.Component {
   render() {
     return (
       <Tabs value={this.state.value} onChange={this.handleChange}>
+        <Tab label="Recommend" value="r">
+          <div>
+            <FriendCardList friendList={this.state.recommendEvents} />
+          </div>
+        </Tab>
         <Tab label="Attended" value="a">
           <div>
             <FriendCardList friendList={this.state.goingEvents} />
