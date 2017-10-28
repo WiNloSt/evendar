8f3a679cdaeb4feb63bac2f49b086cb81fb5c528
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-28 11:52:15 +0700

    Remove unused component

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -5,31 +5,27 @@ import { withState, compose } from 'recompose'
 import { withRouter } from 'react-router-dom'
 
 const Navigation = ({ routes, page, setPage, history }) => (
-  <Wrapper>
-    <StickyBottom>
-      <Paper zDepth={1}>
-        <BottomNavigation selectedIndex={page}>
-          {routes.map((route, index) => (
-            <BottomNavigationItem
-              style={{ fontSize: 16 }}
-              label={route.name}
-              key={route.name}
-              icon={<FontIcon className="material-icons">{route.icon}</FontIcon>}
-              onClick={() => {
-                history.push(route.path)
-                setPage(index)
-              }}
-            />
-          ))}
-        </BottomNavigation>
-      </Paper>
-    </StickyBottom>
-  </Wrapper>
+  <StickyBottom>
+    <Paper zDepth={1}>
+      <BottomNavigation selectedIndex={page}>
+        {routes.map((route, index) => (
+          <BottomNavigationItem
+            style={{ fontSize: 16 }}
+            label={route.name}
+            key={route.name}
+            icon={<FontIcon className="material-icons">{route.icon}</FontIcon>}
+            onClick={() => {
+              history.push(route.path)
+              setPage(index)
+            }}
+          />
+        ))}
+      </BottomNavigation>
+    </Paper>
+  </StickyBottom>
 )
 
 export default compose(withRouter, withState('page', 'setPage', 0))(Navigation)
-
-const Wrapper = styled.div``
 
 const StickyBottom = styled.div`
   width: 100%;
