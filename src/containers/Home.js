15b1816601b834607ba802898ae21ad967b52e20
lifez <lifez999@gmail.com>
2017-10-28 11:32:33 +0700

    Remove debug

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import EventCardList from '../components/EventCardList'
+import {getAllTargetEvents} from '../utils/services'
 
 export default class Home extends React.Component {
+  state = {
+    events: []
+  }
+  componentDidMount = () => {
+    getAllTargetEvents()
+  }
   render() {
+    console.log(this.state.events)
     return (
       <div>
         <EventCardList />
