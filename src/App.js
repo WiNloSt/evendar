c83ca181b792d0b096545851989d4471d34f9a4f
lifez <lifez999@gmail.com>
2017-10-28 09:10:52 +0700

    Add Home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Debug from './Debug'
+import Home from './containers/Home'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <MuiThemeProvider>
         <BrowserRouter>
           <div>
+            <Route exact path="/" component={Home} />
             <Route exact path="/debug" component={Debug} />
           </div>
         </BrowserRouter>
