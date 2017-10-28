83b5db5bfb3df50ee56d64b4d7bb964057fc3d1e
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-28 07:45:44 +0700

    Add emotion (css in js)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
+import React, { Component } from 'react'
+import { Route } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
-import './App.css';
+import './App.css'
 import Debug from './Debug'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <BrowserRouter>
-          <Route exact path="/debug" component={Debug}/>
+          <div>
+            <Route exact path="/debug" component={Debug} />
+          </div>
         </BrowserRouter>
       </MuiThemeProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
