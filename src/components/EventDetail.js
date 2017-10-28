6b46f7146c3b8ef36795ff7a2ff02ea24f7375ca
lifez <lifez999@gmail.com>
2017-10-28 10:41:12 +0700

    Implement button

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import { attend, interest, maybe, cancel } from '../utils/services'
 import * as store from '../store'
 
-const EventDetail = ({ match, id, title, organizerImg, organizerName, description }) => (
+const EventDetail = () => (
   <Card>
   <CardMedia
     overlay={<CardTitle title={store.get().event.name} subtitle={`by ${store.get().event.organizerName}`} />}
@@ -15,9 +16,10 @@ const EventDetail = ({ match, id, title, organizerImg, organizerName, descriptio
     {store.get().event.description}
   </CardText>
   <CardActions style={{textAlign: 'right'}}>
-    <FlatButton label="Go" />
-    <FlatButton label="Interest" />
-    <FlatButton label="Cancel" />
+    <FlatButton label="Attend" onClick={() => { attend(store.get().event.id) }} />
+    <FlatButton label="Maybe" onClick={() => { maybe(store.get().event.id) }} />
+    <FlatButton label="Interest" onClick={() => { interest(store.get().event.id) }} />
+    <FlatButton label="Cancel" onClick={() => { cancel(store.get().event.id) }} />
   </CardActions>
 </Card>
 );
