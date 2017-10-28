57b17490b3bcd97f311bdabe6f305e8b8fe3f25b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-28 09:28:39 +0700

    Add bottom nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom'
+import 'material-design-icons/iconfont/material-icons.css'
+
 import './App.css'
 import Debug from './Debug'
 import Home from './containers/Home'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Navigation from './components/BottomNavigation'
 
 class App extends Component {
   render() {
@@ -12,8 +15,16 @@ class App extends Component {
       <MuiThemeProvider>
         <BrowserRouter>
           <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/debug" component={Debug} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/debug" component={Debug} />
+            </Switch>
+            <Navigation
+              routes={[
+                { path: '/', name: 'Home', icon: 'home' },
+                { path: '/debug', name: 'Event Cards', icon: 'event' }
+              ]}
+            />
           </div>
         </BrowserRouter>
       </MuiThemeProvider>
---
+++ src/components/BottomNavigation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Paper, BottomNavigation, BottomNavigationItem, FontIcon } from 'material-ui'
+import styled from 'react-emotion'
+import { withState, compose } from 'recompose'
+import { withRouter } from 'react-router-dom'
+
+const Navigation = ({ routes, page, setPage, history }) => (
+  <StickyBottom>
+    <Paper zDepth={1}>
+      <BottomNavigation selectedIndex={page}>
+        {routes.map((route, index) => (
+          <BottomNavigationItem
+            style={{ fontSize: 16 }}
+            label={route.name}
+            icon={<FontIcon className="material-icons">{route.icon}</FontIcon>}
+            onClick={() => {
+              history.push(route.path)
+              setPage(index)
+            }}
+          />
+        ))}
+      </BottomNavigation>
+    </Paper>
+  </StickyBottom>
+)
+
+export default compose(withRouter, withState('page', 'setPage', 0))(Navigation)
+
+const StickyBottom = styled.div`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+`
