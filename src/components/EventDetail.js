a76477625127851009f5562051d3e208803ee3dc
lifez <lifez999@gmail.com>
2017-10-28 10:57:07 +0700

    Add Attendee, Maybe, Interest people

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
+
+import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import { attend, interest, maybe, cancel } from '../utils/services'
 import * as store from '../store'
@@ -14,6 +16,18 @@ const EventDetail = () => (
   <CardTitle title="รายละเอียด" subtitle="" />
   <CardText>
     {store.get().event.description}
+    <div>
+    <h3>Attendees</h3>
+    <Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />
+    </div>
+    <div>
+    <h3>Maybe</h3>
+    <Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />
+    </div>
+    <div>
+    <h3>Interested</h3>
+    <Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />
+    </div>
   </CardText>
   <CardActions style={{textAlign: 'right'}}>
     <FlatButton label="Attend" onClick={() => { attend(store.get().event.id) }} />
